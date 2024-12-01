bdb810a7ccc065ed854b2b4188ad0db785bb5716
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2024-12-01 11:04:31 -0500

    Initial Commit

---
+++ MindGrid/MindGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindGridApp.swift
+//  MindGrid
+//
+//  Created by Gerard Gomez on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MindGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MindGridTests/MindGridTests.swift
@@ -0,0 +1,16 @@
+//
+//  MindGridTests.swift
+//  MindGridTests
+//
+//  Created by Gerard Gomez on 12/1/24.
+//
+
+import Testing
+
+struct MindGridTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
